85b11df9fefe0919d4b8212c622c0bcec34acd3f
Adeilson Silva <adeilson.129@gmail.com>
2020-04-20 14:25:39 -0300

    Configurando Header em PHP e chamando o mesmo atraves do Include

---
+++ navigation-list.php
@@ -0,0 +1,4 @@
+<li><a href="index.html#about-us">Sobre</a></li>
+<li><a href="cardapio.html">Cardápio</a></li>
+<li><a href="index.html#contact-us">Reserva</a></li>
+<li><a href="index.html#footer">Contacto</a></li>
